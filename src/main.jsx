42909f2a09cff0ededb8cd36cc65f0ec1e60c1d5
Rnveer007 <rvs523474@gmail.com>
2025-02-03 10:54:56 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,30 @@
  import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
+import  Home from './components/home/Home'
+import About from './components/about/about'
+
+const router = createBrowserRouter ([
+{
+    path : "/",
+    element : <Layout />,
+    children : [
+        {
+            path :  "",
+            element : <Home />
+        },
+        {
+            path : "about",
+            element : <About />
+        }
+    ]
+}
+])
 
 createRoot(document.getElementById('root')).render(
-    <App />
+
+  <RouterProvider router={router} /> 
+
 )
